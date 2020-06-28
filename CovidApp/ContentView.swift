610da38558397524d2e760fe0ae66de6370726bc
Muhammad Debi Sani Abu Bakar Sidik <muhammaddebisani@gmail.com>
2020-06-29 02:43:52 +0700

    Initial Commit

---
+++ CovidApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CovidApp
+//
+//  Created by debi sani on 29/06/20.
+//  Copyright © 2020 debi sani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
